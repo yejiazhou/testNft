c949a902759fe24a746b6a959e2b50a167486ed9
v_jiazye <v_jiazye@tencent.com>
2022-12-31 21:22:07 +0800

    更改

--- _next/static/DCuu2RtW6-_cfqPZlECZ7/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-3b200b38c721baf8.js"],"/_error":["static/chunks/pages/_error-409f831d3504c8f5.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
